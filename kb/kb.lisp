cd96b16f09c016e64615ed7661a4c6c0b869d938
Jonathan Gordon <jgordon@jonathangordon.org>
2017-07-17 14:36:54 -0700

    Update toy example.

--- kb/kb.lisp
+++ kb/kb.lisp
@@ -1,3 +1,7 @@
 (B (name kb01)
-    (=> (steal-vb e1 x y)
-        (criminal-jj e2 x)))
+    (=> (steal-vb e1 x y u)
+        (criminal-nn e2 x)))
+
+(B (name kb02)
+   (=> (kill-vb e1 x y u)
+       (criminal-nn e2 x)))
