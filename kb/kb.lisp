66c0195ddab669f6885ae56460dfb638a9cb9f7e
Jonathan Gordon <jgordon@jonathangordon.org>
2017-09-21 11:51:24 -0700

    Fix costs; add more example axioms.

--- kb/kb.lisp
+++ kb/kb.lisp
@@ -1,11 +1,19 @@
 (B (name kb01)
-    (=> (steal-vb e1 x y u :0.6)
-        (criminal-nn e2 x)))
+    (=> (steal-vb e1 x y u)
+        (criminal-nn e2 x) :0.6))
 
 (B (name kb02)
-   (=> (kill-vb e1 x y u :0.6)
-       (criminal-nn e2 x)))
+   (=> (kill-vb e1 x y u)
+       (criminal-nn e2 x) :0.6))
 
 (B (name kb03)
-   (=> (meal-nn e1 x :0.6)
-       (dinner-nn e2 x)))
+   (=> (meal-nn e1 x)
+       (dinner-nn e2 x) :0.6))
+
+(B (name kb04)
+   (=> (financial-institution e1 x)
+       (bank-nn e2 x) :0.2))
+
+(B (name kb05)
+   (=> (river-bank e1 x)
+       (bank-nn e2 x) :0.8))
